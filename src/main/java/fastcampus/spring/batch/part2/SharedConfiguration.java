43d4eed7f6b4818366bad5dfb61ab4ef59b42e76
prodo-developer <wjdals300@gmail.com>
2021-07-19 20:26:25 +0900

    feat : ExecutionContext 이해 (Job, Step)

---
+++ src/main/java/fastcampus/spring/batch/part2/SharedConfiguration.java
@@ -0,0 +1,76 @@
+package fastcampus.spring.batch.part2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class SharedConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public SharedConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job shareJob() {
+        return jobBuilderFactory.get("shareJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.shareStep())
+                .next(this.shareStep2())
+                .build();
+    }
+
+    @Bean
+    public Step shareStep() {
+        return stepBuilderFactory.get("shareStep")
+                .tasklet((contribution, chunkContext) -> {
+                    StepExecution stepExecution = contribution.getStepExecution();
+                    ExecutionContext stepExecutionContext = stepExecution.getExecutionContext();
+                    stepExecutionContext.putString("stepKey", "step execution context");
+
+                    JobExecution jobExecution = stepExecution.getJobExecution();
+                    JobInstance jobInstance = jobExecution.getJobInstance();
+                    ExecutionContext jobExecutionContext = jobExecution.getExecutionContext();
+                    jobExecutionContext.putString("jobKey", "job execution context");
+                    JobParameters jobParameters = jobExecution.getJobParameters();
+
+                    log.info("jobName : {}, stepName : {}, parameter : {}",
+                            jobInstance.getJobName(),
+                            stepExecution.getStepName(),
+                            jobParameters.getLong("run.id"));
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step shareStep2() {
+        return stepBuilderFactory.get("shareStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    StepExecution stepExecution = contribution.getStepExecution();
+                    ExecutionContext stepExecutionContext = stepExecution.getExecutionContext();
+
+                    JobExecution jobExecution = stepExecution.getJobExecution();
+                    ExecutionContext jobExecutionContext = jobExecution.getExecutionContext();
+
+                    log.info("jobKey : {}, stepKey : {}",
+                            jobExecutionContext.getString("jobKey", "emptyJobKey"),
+                            stepExecutionContext.getString("stepKey", "emptyStepKey"));
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}

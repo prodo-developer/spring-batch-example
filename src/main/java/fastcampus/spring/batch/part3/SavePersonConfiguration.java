ed94c8863e31572339f00dac818db3b387467b25
prodo-developer <wjdals300@gmail.com>
2021-07-26 22:17:19 +0900

    feat : CSV 파일 데이터를 읽어 H2 DB에 데이터 저장하는 배치 개발

---
+++ src/main/java/fastcampus/spring/batch/part3/SavePersonConfiguration.java
@@ -0,0 +1,109 @@
+package fastcampus.spring.batch.part3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.support.CompositeItemWriter;
+import org.springframework.batch.item.support.builder.CompositeItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@Slf4j
+public class SavePersonConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    public SavePersonConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, EntityManagerFactory entityManagerFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Bean
+    public Job savePersonJob() throws Exception {
+        return this.jobBuilderFactory.get("savePersonJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.savePersonStep(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step savePersonStep(@Value("#{jobParameters[allow_duplicate]}") String allowDuplicate) throws Exception {
+        return this.stepBuilderFactory.get("savePersonStep")
+                .<Person, Person>chunk(10)
+                .reader(itemReader())
+                .processor(new DuplicateValidationProcessor<>(Person::getName, Boolean.parseBoolean(allowDuplicate)))
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemWriter<? super Person> itemWriter() throws Exception {
+//        return items -> items.forEach(person -> log.info("로그 {} 확인하기", person.getName()));
+        JpaItemWriter<Person> jpaItemWriter = new JpaItemWriterBuilder<Person>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+
+        ItemWriter<Person> logItemWriter = items -> log.info("person.size : {}", items.size());
+
+        CompositeItemWriter<Person> itemWriter = new CompositeItemWriterBuilder<Person>()
+                .delegates(jpaItemWriter, logItemWriter)
+                .build();
+
+        itemWriter.afterPropertiesSet();
+        return itemWriter;
+    }
+
+    private ItemReader<Person> itemReader() throws Exception {
+        DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setNames("name", "age", "address");
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSet -> {
+            return new Person(
+                fieldSet.readString(0), // 이름
+                fieldSet.readString(1), // 나이
+                fieldSet.readString(2)  // 거주지
+            );
+        });
+        FlatFileItemReader<Person> itemReader = new FlatFileItemReaderBuilder<Person>()
+                .name("savePersonItemReader")
+                .encoding("UTF-8")
+                .linesToSkip(1)
+                .resource(new ClassPathResource("person.csv"))
+                .lineMapper(lineMapper)
+                .build();
+
+        itemReader.afterPropertiesSet();
+        return itemReader;
+    }
+
+//    private List<Person> getItems() {
+//        List<Person> items = new ArrayList<>();
+//
+//        for (int i = 0; i < 10; i++) {
+//            items.add(new Person(i+1,"test name" +i, "test age", "test address"));
+//        }
+//
+//        return items;
+//    }
+}
--- src/main/resources/person.sql
+++ src/main/resources/person.sql
@@ -1,10 +1,10 @@
--- create table person (
---     id bigint primary key auto_increment,
---     name varchar(255),
---     age varchar(255),
---     address varchar(255)
--- );
-insert into spring_batch.person(name, age, address) values('호텔스컴바인','32','원주');
-insert into spring_batch.person(name, age, address) values('야놀자','30','서울');
-insert into spring_batch.person(name, age, address) values('여기어때','25','부산');
+create table person (
+    id bigint primary key auto_increment,
+    name varchar(255),
+    age varchar(255),
+    address varchar(255)
+);
+insert into person(name, age, address) values('호텔스컴바인','32','원주');
+insert into person(name, age, address) values('야놀자','30','서울');
+insert into person(name, age, address) values('여기어때','25','부산');
 

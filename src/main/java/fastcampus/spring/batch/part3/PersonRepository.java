a55263ea9b86f5002de110ffb3b943597e446bd0
prodo-developer <wjdals300@gmail.com>
2021-07-27 01:40:09 +0900

    feat : Person 레포지토리 인터페이스 추가

---
+++ src/main/java/fastcampus/spring/batch/part3/PersonRepository.java
@@ -0,0 +1,7 @@
+package fastcampus.spring.batch.part3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+}

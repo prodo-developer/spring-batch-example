ade6aa5c72c16017f3b247e092683d71c0ad616d
prodo-developer <wjdals300@gmail.com>
2021-08-01 22:00:53 +0900

    refactor : itemReader, itemProcessor, itemWriter 추가

--- src/main/java/fastcampus/spring/batch/part4/UserConfiguration.java
+++ src/main/java/fastcampus/spring/batch/part4/UserConfiguration.java
@@ -1,5 +1,6 @@
 package fastcampus.spring.batch.part4;
 
+import fastcampus.spring.batch.part4.domain.User;
 import fastcampus.spring.batch.part4.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -7,8 +8,15 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @Slf4j
@@ -17,11 +25,13 @@ public class UserConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final UserRepository userRepository;
+    private final EntityManagerFactory entityManagerFactory;
 
-    public UserConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, UserRepository userRepository) {
+    public UserConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, UserRepository userRepository, EntityManagerFactory entityManagerFactory) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
         this.userRepository = userRepository;
+        this.entityManagerFactory = entityManagerFactory;
     }
 
     @Bean
@@ -37,5 +47,45 @@ public class UserConfiguration {
         return this.stepBuilderFactory.get("customItemReaderStep")
                 .tasklet(new SaveUserTasklet(userRepository))
                 .build();
+    }
+
+    @Bean
+    public Step userLevelUpStep() throws Exception {
+        return this.stepBuilderFactory.get("userLevelUpStep")
+                .<User, User>chunk(100)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemWriter<? super User> itemWriter() {
+        return users -> users.forEach(x -> {
+                x.levelUp();
+                userRepository.save(x);
+        });
+    }
+
+    private ItemProcessor<? super User, ? extends User> itemProcessor() {
+        return user -> {
+            if (user.availabelLevelUp()) {
+                return user;
+            }
+
+            return null;
+        };
+    }
+
+    private ItemReader<? extends User> itemReader() throws Exception {
+        JpaPagingItemReader<User> itemReader = new JpaPagingItemReaderBuilder<User>()
+                .queryString("select u from User u")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(100)
+                .name("userItemReader")
+                .build();
+
+        itemReader.afterPropertiesSet();
+
+        return itemReader;
     }
 }

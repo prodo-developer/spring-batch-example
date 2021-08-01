8875e4257e1d14d6cf5263e1924ecb80798f4590
prodo-developer <wjdals300@gmail.com>
2021-08-01 21:59:52 +0900

    fix : Level 케이스 추가

--- src/main/java/fastcampus/spring/batch/part4/domain/Level.java
+++ src/main/java/fastcampus/spring/batch/part4/domain/Level.java
@@ -1,5 +1,7 @@
 package fastcampus.spring.batch.part4.domain;
 
+import java.util.Objects;
+
 public enum Level {
     VIP(500_000, null),
     GOLD(500_000, VIP),
@@ -12,5 +14,40 @@ public enum Level {
     Level(int nextAmount, Level nextLevel) {
         this.nextAmount = nextAmount;
         this.nextLevel = nextLevel;
+    }
+
+    public static boolean availabelLevelUp(Level level, int totalAmount) {
+        if(Objects.isNull(level)) {
+            return false;
+        }
+
+        if(Objects.isNull(level.nextLevel)) {
+            return false;
+        }
+
+        return totalAmount >= level.nextAmount;
+    }
+
+    static Level getNextLevel(int totalAmount) {
+        if(totalAmount >= Level.VIP.nextAmount) {
+            return VIP;
+        }
+
+        // GOLD.nextLevel == VIP
+        if(totalAmount >= Level.GOLD.nextAmount) {
+            return GOLD.nextLevel;
+        }
+
+        // SILVER.nextLevel == GOLD
+        if(totalAmount >= Level.SILVER.nextAmount) {
+            return SILVER.nextLevel;
+        }
+
+        // NORMAL.nextLevel == SILVER
+        if(totalAmount >= Level.NORMAL.nextAmount) {
+            return NORMAL.nextLevel;
+        }
+
+        return NORMAL;
     }
 }

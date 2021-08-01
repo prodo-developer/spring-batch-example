c092203424f0ebdbafb642eca4799e85bba2c23c
prodo-developer <wjdals300@gmail.com>
2021-08-02 01:49:40 +0900

    refactor : Orders 관계 형성을 위한 리팩토링

--- src/main/java/fastcampus/spring/batch/part4/SaveUserTasklet.java
+++ src/main/java/fastcampus/spring/batch/part4/SaveUserTasklet.java
@@ -2,17 +2,21 @@ package fastcampus.spring.batch.part4;
 
 import fastcampus.spring.batch.part4.domain.User;
 import fastcampus.spring.batch.part4.repository.UserRepository;
+import fastcampus.spring.batch.part5.domain.Orders;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class SaveUserTasklet implements Tasklet {
 
+    private final int ZERO = 0;
+    private final int SIZE = 100;
     private final UserRepository userRepository;
 
     public SaveUserTasklet(UserRepository userRepository) {
@@ -33,33 +37,49 @@ public class SaveUserTasklet implements Tasklet {
     private List<User> createUsers() {
         List<User> users = new ArrayList<>();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = ZERO; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(1_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(1_000)
+                            .createDate(LocalDate.of(2021,8,1))
+                            .itemName("item" +i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
 
         // SILVER 등급
-       for (int i = 100; i < 200; i++) {
+       for (int i = ZERO; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(200_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(200_000)
+                            .createDate(LocalDate.of(2021,8,2))
+                            .itemName("item" +i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
 
         // GOLD 등급
-        for (int i = 200; i < 300; i++) {
+        for (int i = ZERO; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(300_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(300_000)
+                            .createDate(LocalDate.of(2021,8,3))
+                            .itemName("item" +i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
 
         // VIP 등급
-        for (int i = 300; i < 400; i++) {
+        for (int i = ZERO; i < SIZE; i++) {
             users.add(User.builder()
-                    .totalAmount(500_000)
+                    .orders(Collections.singletonList(Orders.builder()
+                            .amount(500_000)
+                            .createDate(LocalDate.of(2021,8,4))
+                            .itemName("item" +i)
+                            .build()))
                     .username("test username" + i)
                     .build());
         }
--- src/main/java/fastcampus/spring/batch/part4/domain/User.java
+++ src/main/java/fastcampus/spring/batch/part4/domain/User.java
@@ -1,5 +1,6 @@
 package fastcampus.spring.batch.part4.domain;
 
+import fastcampus.spring.batch.part5.domain.Orders;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Entity
@@ -21,18 +23,32 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Level level = Level.NORMAL;
 
-    private int totalAmount;
+//    private int totalAmount;
+    /**
+     * 1:N 관계
+     * user는 n개의 Orders를 가질 수 있다.
+     * user가 저장되면서 Orders를 같이 저장할 수 있도록 영속성 전이(PERSIST)를 적용한다.
+    **/
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user_id")
+    private List<Orders> orders;
 
     private LocalDate updatedDate;
 
     @Builder
-    private User(String username, int totalAmount) {
+    private User(String username, List<Orders> orders) {
         this.username = username;
-        this.totalAmount = totalAmount;
+        this.orders = orders;
     }
 
     public boolean availableLeveUp() {
         return Level.availableLevelUp(this.getLevel(), this.getTotalAmount());
+    }
+
+    private int getTotalAmount() {
+        return this.orders.stream()
+                .mapToInt(Orders::getAmount)
+                .sum();
     }
 
     public Level levelUp() {

2db8761b11f7c072e65a3a1a2ff83eddf947a0ed
prodo-developer <wjdals300@gmail.com>
2021-08-04 01:33:45 +0900

    refactor : FetchType.EAGER 변경

--- src/main/java/fastcampus/spring/batch/part4/domain/User.java
+++ src/main/java/fastcampus/spring/batch/part4/domain/User.java
@@ -28,8 +28,12 @@ public class User {
      * 1:N 관계
      * user는 n개의 Orders를 가질 수 있다.
      * user가 저장되면서 Orders를 같이 저장할 수 있도록 영속성 전이(PERSIST)를 적용한다.
+     *
+     * 멀티쓰레드 환경에서 여러 쓰레드가 Lazy설정 된 엔티티로 접근하게 되면 Lazy로딩 에러가 발생할 수 있음
+     * fetch = FetchType.EAGER 적용을 통해 해결
+     * user와 orders의 항상 조회하는 경우에만 사용을 했지만 되도록이면 지연로딩을 권장한다.
     **/
-    @OneToMany(cascade = CascadeType.PERSIST)
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private List<Orders> orders;
 

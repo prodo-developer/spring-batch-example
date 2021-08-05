116031692f4d3ecdfe7bef5ca8650c2ec5a055dc
prodo-developer <wjdals300@gmail.com>
2021-08-05 23:44:27 +0900

    refactor : 최소, 최대값 추가

--- src/main/java/fastcampus/spring/batch/part4/repository/UserRepository.java
+++ src/main/java/fastcampus/spring/batch/part4/repository/UserRepository.java
@@ -2,10 +2,17 @@ package fastcampus.spring.batch.part4.repository;
 
 import fastcampus.spring.batch.part4.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.Collection;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Collection<User> findAllByUpdatedDate(LocalDate updatedDate);
+
+    @Query(value = "select min(u.id) from User u")
+    long findMinId();
+
+    @Query(value = "select max(u.id) from User u")
+    long findMaxId();
 }

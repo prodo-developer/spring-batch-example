b52459fc61c6379479c70f0256d0b4e0088389b3
prodo-developer <wjdals300@gmail.com>
2021-08-06 19:46:07 +0900

    feat : ParallelStep 추가 및 성능 측정

---
+++ src/main/java/fastcampus/spring/batch/part6/ParallelUserConfiguration.java
@@ -0,0 +1,222 @@
+package fastcampus.spring.batch.part6;
+
+import fastcampus.spring.batch.part4.LevelUpJobExecutionListener;
+import fastcampus.spring.batch.part4.SaveUserTasklet;
+import fastcampus.spring.batch.part4.domain.User;
+import fastcampus.spring.batch.part4.repository.UserRepository;
+import fastcampus.spring.batch.part5.JobParameterDecide;
+import fastcampus.spring.batch.part5.OrderStatistics;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.support.SimpleFlow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.TaskExecutor;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Slf4j
+public class ParallelUserConfiguration {
+
+    private final String JOB_NAME = "parallelUserJob";
+    public static final int CHUNK_SIZE = 1000;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final UserRepository userRepository;
+    private final EntityManagerFactory entityManagerFactory;
+    private final DataSource dataSource;
+    private final TaskExecutor taskExecutor;
+
+    public ParallelUserConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, UserRepository userRepository, EntityManagerFactory entityManagerFactory, DataSource dataSource, TaskExecutor taskExecutor) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.userRepository = userRepository;
+        this.entityManagerFactory = entityManagerFactory;
+        this.dataSource = dataSource;
+        this.taskExecutor = taskExecutor;
+    }
+
+    @Bean(JOB_NAME)
+    public Job userJob() throws Exception {
+        return this.jobBuilderFactory.get(JOB_NAME)
+                .incrementer(new RunIdIncrementer())
+                .listener(new LevelUpJobExecutionListener(userRepository))
+                .start(this.saveUserFlow())
+                .next(this.splitFlow(null))
+                .build()
+                .build();
+    }
+
+    @Bean(JOB_NAME + "_saveUserFlow")
+    public Flow saveUserFlow() {
+        TaskletStep saveUserStep = this.stepBuilderFactory.get(JOB_NAME + "_saveUserStep")
+                .tasklet(new SaveUserTasklet(userRepository))
+                .build();
+
+        return new FlowBuilder<SimpleFlow>(JOB_NAME + "_saveUserSlow")
+                .start(saveUserStep)
+                .build();
+    }
+
+    @Bean(JOB_NAME + "_splitFlow")
+    @JobScope
+    public Flow splitFlow(@Value("#{jobParameters[date]}") String date) throws Exception {
+        Flow userLevelUpFlow = new FlowBuilder<SimpleFlow>(JOB_NAME + "_userLevelUpFlow")
+                .start(userLevelUpStep())
+                .build();
+
+        // orderStatisticsFlow을 사용함으로써 아래의 Bean 객체에 등록 할 필요가 없다.
+        // step 2개를 하나로 만들기 위해 splitFlow가 userLevelUpFlow와 orderStatisticsFlow를 병렬로 처리함.
+        return new FlowBuilder<SimpleFlow>(JOB_NAME + "_splitFlow")
+                .split(this.taskExecutor)
+                .add(userLevelUpFlow, orderStatisticsFlow(date))
+                .build();
+    }
+
+    /**
+     * @param date
+     * @return
+     *
+     * userJob에 있는 step을 분리하여 Flow에 구현한다.
+     */
+    private Flow orderStatisticsFlow(String date) throws Exception {
+        return new FlowBuilder<SimpleFlow>(JOB_NAME + "_orderStatisticsFlow")
+                .start(new JobParameterDecide("date")) // 가져온값이 아래의 CONTINUE인지 체크
+                .on(JobParameterDecide.CONTINUE.getName()) // CONTINUE이면 아래의 to메서드 실행
+                .to(this.orderStatisticsStep(date))
+                .build();
+    }
+
+    public Step orderStatisticsStep(@Value("#{jobParameters[date]}") String date) throws Exception {
+        return this.stepBuilderFactory.get(JOB_NAME + "_orderStatisticsStep")
+                .<OrderStatistics, OrderStatistics>chunk(CHUNK_SIZE)
+                .reader(orderStatisticsItemReader(date))
+                .writer(orderStatisticsItemWriter(date))
+                .build();
+    }
+
+    private ItemReader<? extends OrderStatistics> orderStatisticsItemReader(String date) throws Exception {
+        YearMonth yearMonth = YearMonth.parse(date);
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("startDate", yearMonth.atDay(1)); // 2021년 8월 1일
+        parameters.put("endDate", yearMonth.atEndOfMonth()); // 8월의 마지막 날
+
+        Map<String, Order> sortKey = new HashMap<>();
+        sortKey.put("created_date", Order.ASCENDING);
+
+        JdbcPagingItemReader<OrderStatistics> itemReader = new JdbcPagingItemReaderBuilder<OrderStatistics>()
+                .dataSource(this.dataSource)
+                .rowMapper((resultSet, i) -> OrderStatistics.builder()
+                        .amount(resultSet.getString(1))
+                        .date(LocalDate.parse(resultSet.getString(2), DateTimeFormatter.ISO_DATE))
+                        .build())
+                .pageSize(CHUNK_SIZE) // 페이징 설정
+                .name(JOB_NAME + "_orderStatisticsItemReader")
+                .selectClause("sum(amount), created_date")
+                .fromClause("orders")
+                .whereClause("created_date >= :startDate and created_date <= :endDate")
+                .groupClause("created_date")
+                .parameterValues(parameters)
+                .sortKeys(sortKey)
+                .build();
+
+        itemReader.afterPropertiesSet();
+
+        return itemReader;
+    }
+
+    private ItemWriter<? super OrderStatistics> orderStatisticsItemWriter(String date) throws Exception {
+        YearMonth yearMonth = YearMonth.parse(date);
+
+        String fileName = yearMonth.getYear() + "년_" + yearMonth.getMonthValue() + "월_일별_주문_금액.csv";
+
+        BeanWrapperFieldExtractor<OrderStatistics> fieldExtractor = new BeanWrapperFieldExtractor<>();
+        fieldExtractor.setNames(new String[]{"amount", "date"});
+
+        DelimitedLineAggregator<OrderStatistics> lineAggregator = new DelimitedLineAggregator<>();
+        lineAggregator.setDelimiter(",");
+        lineAggregator.setFieldExtractor(fieldExtractor);
+
+        FlatFileItemWriter<OrderStatistics> itemWriter = new FlatFileItemWriterBuilder<OrderStatistics>()
+                .resource(new FileSystemResource("output/" + fileName))
+                .lineAggregator(lineAggregator)
+                .name(JOB_NAME + "_orderStatisticsItemWriter")
+                .encoding("UTF-8")
+                .headerCallback(writer -> writer.write("total_amount,date"))
+                .build();
+
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+    }
+
+    @Bean(JOB_NAME + "_userLevelUpStep")
+    public Step userLevelUpStep() throws Exception {
+        return this.stepBuilderFactory.get(JOB_NAME + "_userLevelUpStep")
+                .<User, User>chunk(CHUNK_SIZE)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private ItemWriter<? super User> itemWriter() {
+        return users -> users.forEach(x -> {
+                x.levelUp();
+                userRepository.save(x);
+        });
+    }
+
+    private ItemProcessor<? super User, ? extends User> itemProcessor() {
+        return user -> {
+            if (user.availableLeveUp()) {
+                return user;
+            }
+
+            return null;
+        };
+    }
+
+    private ItemReader<? extends User> itemReader() throws Exception {
+        JpaPagingItemReader<User> itemReader = new JpaPagingItemReaderBuilder<User>()
+                .queryString("select u from User u")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(CHUNK_SIZE)
+                .name(JOB_NAME + "_userItemReader")
+                .build();
+
+        itemReader.afterPropertiesSet();
+
+        return itemReader;
+    }
+}
